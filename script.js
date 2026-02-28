af5a1d304282d294da53d4c68dde031767955201
mkasendar09 <mkasendar09@gmail.com>
2026-02-28 16:06:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const playBtn = document.getElementById("playBtn");
+const music = document.getElementById("bgMusic");
 
+playBtn.addEventListener("click", () => {
+    if (music.paused) {
+        music.play();
+        playBtn.textContent = "Pause 🎵";
+    } else {
+        music.pause();
+        playBtn.textContent = "Play 🎵";
+    }
+});
